9519605f518883a29f6549cea5256254af9b568e
Shuvarup66 <shuvarup26@gmail.com>
2025-09-07 03:55:19 +0530

    url updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-   origin: "https://68bcaef7cd486acc359d99a4--funny-bavarois-c2716c.netlify.app",
+   origin: "https://funny-bavarois-c2716c.netlify.app",
     credentials: true     
 }))
 const PORT = process.env.PORT || 5000;
